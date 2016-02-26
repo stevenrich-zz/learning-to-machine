5e8524a8a7cc55d92e214268cbf7ac5d2e5f3d76
Steven Rich <steven.rich@washpost.com>
2016-02-25 20:28:42 -0500

    Create intro.py

---
+++ intro.py
@@ -0,0 +1,5 @@
+from sklearn import preprocessing
+from sklearn import cross_validation
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
